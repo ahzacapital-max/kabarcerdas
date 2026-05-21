8c20e44d21dea959bb496b2cc2887aee8dc46b58
CVGenius v1 - YudVi Nexa Labs <yuda0806@gmail.com>
2026-05-21 12:16:51 +0700

    feat: favicon KabarCerdas

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="id" className={`${lora.variable} ${dmSans.variable}`}>
       <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="sitemap" href="/api/sitemap" />
       </head>
       <body>
